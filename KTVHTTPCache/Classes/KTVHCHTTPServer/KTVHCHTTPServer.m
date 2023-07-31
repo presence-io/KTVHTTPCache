b2991b0472caf4b6a813a9aa7945cc2c29cec660
Jin Xing <lilely521@gmail.com>
2023-07-31 15:22:07 +0800

    change default port to 8765

--- KTVHTTPCache/Classes/KTVHCHTTPServer/KTVHCHTTPServer.m
+++ KTVHTTPCache/Classes/KTVHCHTTPServer/KTVHCHTTPServer.m
@@ -39,7 +39,7 @@
         self.server = [[HTTPServer alloc] init];
         [self.server setConnectionClass:[KTVHCHTTPConnection class]];
         [self.server setType:@"_http._tcp."];
-        [self.server setPort:80];
+        [self.server setPort:8765];
         self.backgroundTask = UIBackgroundTaskInvalid;
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(applicationDidEnterBackground)
